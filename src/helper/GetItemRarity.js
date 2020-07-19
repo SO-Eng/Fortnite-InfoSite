acf98beda143c43eabb867665301779d09c472da
SoftwOrt Engineering <markus.sczeburek-ort@softwort-engineering.com>
2020-07-19 21:05:27 +0200

    Added all Rarity types to Project

--- src/helper/GetItemRarity.js
+++ src/helper/GetItemRarity.js
@@ -1,16 +1,40 @@
 const getItemRarity = (rarity) => {
     let rareColor = "";
     switch(rarity) {
-        case "uncommon":
+        case "handmade":
+            return "cardHandmade";
+        case "common":
             return "cardCommon";
+        case "uncommon":
+            return "cardUncommon";
         case "rare":
             return "cardRare";
         case "epic":
             return "cardEpic";
+        case "legendary":
+            return "cardLegendary";
+        case "mythic":
+            return "cardMythic";
+        case "transcendent":
+            return "cardThranscendent";
+        case "marvel":
+            return "cardMarvel";
+        case "dark series":
+            return "cardDark";
         case "dc":
             return "cardDc";
+        case "frozen series":
+            return "cardFrozen";
+        case "lava series":
+            return "cardLava";
+        case "icon series":
+            return "cardIcon";
         case "shadow series":
             return "cardShadow";
+        case "star wars":
+            return "cardStarwars";
+        case "slurp series":
+            return "cardSlurp";
     };
 };
 
