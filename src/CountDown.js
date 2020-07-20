989a2ac6b896e00c242fbf23fb5b0c7a72b0e75c
SoftwOrt Engineering <markus.sczeburek-ort@softwort-engineering.com>
2020-07-20 14:41:29 +0200

    Finalyzed Countdown Timer

--- src/CountDown.js
+++ src/CountDown.js
@@ -34,30 +34,118 @@ class Countdown extends React.Component {
     render() {
         const { hours, minutes, seconds } = this.state;
 
+        // Mapping the date values to radius values
+        //const daysRadius = mapNumber(days, 30, 0, 0, 360);
+        const hoursRadius = mapNumber(hours, 0, 24, 0, 360);
+        const minutesRadius = mapNumber(minutes, 0, 60, 0, 360);
+        const secondsRadius = mapNumber(seconds, 0, 60, 0, 360);
+
+        if (!seconds) {
+            return null;
+        }
+
         return (
             <div>
-                <h1>Countdown</h1>
+                <h1>Countdown für neue Items</h1>
                 <div className="countdown-wrapper">
-                    {/* <div className="countdown-item">
-                        {days}
-                        <span>days</span>
-                    </div> */}
-                    <div className="countdown-item">
-                        {hours}
-                        <span>hours</span>
-                    </div>
-                    <div className="countdown-item">
-                        {minutes}
-                        <span>minutes</span>
-                    </div>
-                    <div className="countdown-item">
-                        {seconds}
-                        <span>seconds</span>
-                    </div>
+                    {hours && (
+                        <div className="countdown-item">
+                            <SVGCircleHours radius={hoursRadius} />
+                            {hours}
+                            <span>stunden</span>
+                        </div>
+                    )}
+                    {minutes && (
+                        <div className="countdown-item">
+                            <SVGCircleMinutes radius={minutesRadius} />
+                            {minutes}
+                            <span>minuten</span>
+                        </div>
+                    )}
+                    {seconds && (
+                        <div className="countdown-item">
+                            <SVGCircleSeconds radius={secondsRadius} />
+                            {seconds}
+                            <span>sekonden</span>
+                        </div>
+                    )}
                 </div>
             </div>
         );
     };
+}
+
+const SVGCircleHours = ({ radius }) => (
+    <svg className="countdown-svg">
+        <path
+            fill="none"
+            stroke="#E48822"
+            strokeWidth="6"
+            d={describeArc(50, 50, 47, 0, radius)}
+        />
+    </svg>
+);
+
+const SVGCircleMinutes = ({ radius }) => (
+    <svg className="countdown-svg">
+        <path
+            fill="none"
+            stroke="#B954F4"
+            strokeWidth="6"
+            d={describeArc(50, 50, 47, 0, radius)}
+        />
+    </svg>
+);
+
+const SVGCircleSeconds = ({ radius }) => (
+    <svg className="countdown-svg">
+        <path
+            fill="none"
+            stroke="#58AEE6"
+            strokeWidth="6"
+            d={describeArc(50, 50, 47, 0, radius)}
+        />
+    </svg>
+);
+
+// From StackOverflow: https://stackoverflow.com/questions/5736398/how-to-calculate-the-svg-path-for-an-arc-of-a-circle
+function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+    var angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
+
+    return {
+        x: centerX + radius * Math.cos(angleInRadians),
+        y: centerY + radius * Math.sin(angleInRadians)
+    };
+}
+
+function describeArc(x, y, radius, startAngle, endAngle) {
+    var start = polarToCartesian(x, y, radius, endAngle);
+    var end = polarToCartesian(x, y, radius, startAngle);
+
+    var largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
+
+    var d = [
+        'M',
+        start.x,
+        start.y,
+        'A',
+        radius,
+        radius,
+        0,
+        largeArcFlag,
+        0,
+        end.x,
+        end.y
+    ].join(' ');
+
+    return d;
+}
+
+// From StackOverflow: https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+function mapNumber(number, in_min, in_max, out_min, out_max) {
+    return (
+        ((number - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+    );
 }
 
 export default Countdown
