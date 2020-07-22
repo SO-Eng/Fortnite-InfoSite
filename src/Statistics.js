97fe7633820a12b0d2cae6cd2b5fc6a1c4991775
SoftwOrt Engineering <markus.sczeburek-ort@softwort-engineering.com>
2020-07-22 21:53:41 +0200

    Added Select to searchBar

--- src/Statistics.js
+++ src/Statistics.js
@@ -9,44 +9,70 @@ function Statistics() {
   var requestOptions = {
       headers: myHeaders,
   };
-  
+
+
   const [search, setSearch] = useState("");
   const [query, setquery] = useState("");
   const [playerId, setPlayerId] = useState("");
   const [playerStats, setPlayerStats] = useState([]);
-
+  const [platform, setPlatform] = useState("");
+  
+  
   useEffect(() => {
     if (query != "") {
-      fetchItems();
+      //fetchItems();
     }
   }, [query]);
-
+  
   const fetchItems = async () => {
-      const data = await fetch(`https://fortniteapi.io/lookup?username=Ipac_Melvin&platform=epic`, requestOptions);
-
+    if (query == "") {
+      return;
+    }
+    if (platform != "epic") {
+      const data = await fetch(`https://fortniteapi.io/lookup?username=${query}&platform=${platform}`, requestOptions);
       const items = await data.json();
-
+    
       console.log(items);
       setPlayerId(items)
-
+      
       const data2 = await fetch(`https://fortniteapi.io/stats?account=${items.account_id}`, requestOptions);
       const userStats = await data2.json();
-
+      
       console.log(userStats);
       setPlayerStats(userStats);
+    }
+    else{
+      const data = await fetch(`https://fortniteapi.io/lookup?username=${query}`, requestOptions);
+      const items = await data.json();
+    
+      console.log(items);
+      setPlayerId(items)
+      
+      const data2 = await fetch(`https://fortniteapi.io/stats?account=${items.account_id}`, requestOptions);
+      const userStats = await data2.json();
+      
+      console.log(userStats);
+      setPlayerStats(userStats);
+    }
   };
-
+  
   const updateSearch = e => {
     setSearch(e.target.value);
   };
-
+  
   const getSearch = e => {
     e.preventDefault();
     setquery(search);
-    setSearch("");
+    fetchItems();
+    //setSearch("");
   };
-
-
+  
+  const getSelection = e => {
+    let {value} = e.target;
+    console.log(value);
+    setPlatform(value);
+    };
+  
   return (
     <div>
       <div className="siteInfoBar">
@@ -55,11 +81,16 @@ function Statistics() {
       <div className="shopCard">
         <form onSubmit={getSearch} className="searchForm">
           <input className="searchBar" type="text" value={search} onChange={updateSearch} />
+          <select className="selectBar" onChange={getSelection}>
+            <option value="epic">Epic</option>
+            <option value="xbl">X-Box</option>
+            <option value="psn">PlayStation</option>
+          </select>
           <button className="searchButton" type="submit" >Suche</button>
         </form>
       </div>
       <div className="shopCard">
-        <h3>Sorry, this Account is private!</h3>
+        <h1>Hier kommen bald die Infos!!!</h1>
       </div>
     </div>
   );
