244f6ef8ffa41fcdbff1b2d50b181dcffdc4a80f
SoftwOrt Engineering <markus.sczeburek-ort@softwort-engineering.com>
2020-07-22 20:32:36 +0200

    Added Searchbar to search for Player

--- src/Statistics.js
+++ src/Statistics.js
@@ -10,28 +10,40 @@ function Statistics() {
       headers: myHeaders,
   };
   
-  useEffect(() => {
-      fetchItems();
-  }, []);
-
-  const [playerId, setPlayerId] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setquery] = useState("");
+  const [playerId, setPlayerId] = useState("");
   const [playerStats, setPlayerStats] = useState([]);
 
+  useEffect(() => {
+    if (query != "") {
+      fetchItems();
+    }
+  }, [query]);
+
   const fetchItems = async () => {
-      const data = await fetch('https://fortniteapi.io/lookup?username=Ipac_Melvin', requestOptions);
+      const data = await fetch(`https://fortniteapi.io/lookup?username=Ipac_Melvin&platform=epic`, requestOptions);
 
       const items = await data.json();
 
-      console.log(items.account_id);
-      setPlayerId(items.account_id)
+      console.log(items);
+      setPlayerId(items)
 
-      if (playerId != null) {
-          const data2 = await fetch(`https://fortniteapi.io/stats?account=${items.account_id}`, requestOptions);
-          const items2 = await data2.json();
-  
-          console.log(items2);
-          setPlayerStats(items2);
-      }
+      const data2 = await fetch(`https://fortniteapi.io/stats?account=${items.account_id}`, requestOptions);
+      const userStats = await data2.json();
+
+      console.log(userStats);
+      setPlayerStats(userStats);
+  };
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setquery(search);
+    setSearch("");
   };
 
 
@@ -41,7 +53,10 @@ function Statistics() {
         <h3>Hier kannst Du Playerstatistiken einsehen!</h3>
       </div>
       <div className="shopCard">
-        <h1>Name: {playerStats.name}</h1>
+        <form onSubmit={getSearch} className="searchForm">
+          <input className="searchBar" type="text" value={search} onChange={updateSearch} />
+          <button className="searchButton" type="submit" >Suche</button>
+        </form>
       </div>
       <div className="shopCard">
         <h3>Sorry, this Account is private!</h3>
