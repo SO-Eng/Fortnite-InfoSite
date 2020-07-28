8c58f4c4013df16b238912bdc4342d04ea6ba97d
SoftwOrt Engineering <markus.sczeburek-ort@softwort-engineering.com>
2020-07-28 23:16:28 +0200

    Design finalized for UserStats

--- src/UserStats.js
+++ src/UserStats.js
@@ -35,23 +35,47 @@ function UserStats({ stats, game }) {
             </div>
             <div className="statsList">
                 <div>
-                    <div className="statsBox">
-                        <div className="statsKey">Wins</div>
-                        <div className="statsValue">{stats?.placetop1}</div>
+                    <div>
+                        <div className="statsBox">
+                            <div className="statsKey">Wins</div>
+                            <div className="statsValue">{stats?.placetop1}</div>
+                        </div>
+                        <div className="statsBox">
+                            <div className="statsKey">Kills</div>
+                            <div className="statsValue">{stats?.kills}</div>
+                        </div>
                     </div>
-                    <div className="statsBox">
-                        <div className="statsKey">Kills</div>
-                        <div className="statsValue">{stats?.kills}</div>
+                    <div>
+                        <div className="statsBox">
+                            <div className="statsKey">Punkte</div>
+                            <div className="statsValue">{stats?.score}</div>
+                        </div>
+                        <div className="statsBox">
+                            <div className="statsKey">Top 10 Platzierung</div>
+                            <div className="statsValue">{(stats?.placetop1 + stats?.placetop3 + stats?.placetop5 + stats?.placetop6 + stats?.placetop10)}</div>
+                        </div>
                     </div>
                 </div>
                 <div>
-                    <div className="statsBox">
-                        <div className="statsKey">Win %</div>
-                        <div className="statsValue">{(stats?.winrate)*100}</div>
+                    <div>
+                        <div className="statsBox">
+                            <div className="statsKey">Siegrate</div>
+                            <div className="statsValue">{(stats?.winrate)*100}</div>
+                        </div>
+                        <div className="statsBox">
+                            <div className="statsKey">K/D</div>
+                            <div className="statsValue">{stats?.kd}</div>
+                        </div>
                     </div>
-                    <div className="statsBox">
-                        <div className="statsKey">K/D</div>
-                        <div className="statsValue">{stats?.kd}</div>
+                    <div>
+                        <div className="statsBox">
+                            <div className="statsKey">Überlebte Gegner</div>
+                            <div className="statsValue">{stats?.playersoutlived}</div>
+                        </div>
+                        <div className="statsBox">
+                            <div className="statsKey">Gespielte Minuten</div>
+                            <div className="statsValue">{stats?.minutesplayed}</div>
+                        </div>
                     </div>
                 </div>
             </div>
